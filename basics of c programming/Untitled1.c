d497580f6e6d7ee29446eaff994a710dd5bc0f80
hitayezujeanpierre <hitlapidis2@gmail.com>
2023-01-28 12:59:24 +0200

    even or old

---
+++ basics of c programming/Untitled1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{ int x;
+printf("please enter your number");
+scanf("%d",&x);
+if(x%2=0)
+{ printf("the number is even \n");
+
+}
+else
+{
+printf("the number is odd");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
